d0411b0aa4a03067ec5ae4e6f1ac53f4bf17e44b
UshasriGajal <ushasrigajal@gmail.com>
2024-01-31 15:34:05 +0530

    first commit

---
+++ Portfolio.js
@@ -0,0 +1,68 @@
+function profilebut(){
+    window.location.href="https://github.com/UshasriGajal"
+}
+function Repositorybut(){
+    window.location.href="https://github.com/UshasriGajal?tab=repositories"
+}
+function linkedin(){
+    window.location.href="https://www.linkedin.com/in/ushasri-gajal-59b09324b/x"
+}
+function moviebut(){
+    window.location.href="https://ushasrigajal.github.io/MovieApi/"
+}
+function moviecode(){
+    window.location.href="https://github.com/UshasriGajal/MovieApi.git"
+}
+function currencybut(){
+    window.location.href="https://ushasrigajal.github.io/CurrencyConverter/"
+}
+function currencycode(){
+    window.location.href="https://github.com/UshasriGajal/CurrencyConverter.git"
+}
+function ticbut(){
+    window.location.href="https://ushasrigajal.github.io/TicTacToeGame/"
+}
+function ticcode(){
+    window.location.href="https://github.com/UshasriGajal/TicTacToeGame.git"
+}
+function Typebut(){
+    window.location.href="https://ushasrigajal.github.io/Typing_Test/"
+}
+function Typecode(){
+    window.location.href="https://github.com/UshasriGajal/Typing_Test.git"
+}
+function Zomatobut(){
+    window.location.href="https://cjz648.csb.app/"
+}
+function Zomatocode(){
+    window.location.href="https://codesandbox.io/p/sandbox/zomato-cjz648"
+}
+function navitem(){
+    console.log("yes")
+    var x = document.getElementById("portbut");
+    var divi=document.createElement("div")
+    divi.setAttribute("class","divi")
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+// var elee=document.getElementById("projects").innerText
+// window.onload = function(){
+//     document.getElementById("projects").style.width="1100px"
+//     document.getElementById("projects").style.height="800px"
+
+    // project.innerText="hello"
+    // var image=document.createAttribute("img")
+    // image.src
+    // project.setAttribute("class","projectsdiv")
+    // var divi=document.createElement("div")
+    // divi.setAttribute("class","divi")
+    // project.append(divi)
+    // // document.body.append(project)
+    // console.log("yes")
+// }
+// function Resumebut(){
+
+// }
