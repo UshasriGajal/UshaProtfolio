bd30a2d99da2548ef9865f91d8c80f2c4a4fec6c
UshasriGajal <126794491+UshasriGajal@users.noreply.github.com>
2024-04-04 13:13:37 +0530

    Update Portfolio.js

--- Portfolio.js
+++ Portfolio.js
@@ -1,68 +1,25 @@
-function profilebut(){
-    window.location.href="https://github.com/UshasriGajal"
-}
-function Repositorybut(){
-    window.location.href="https://github.com/UshasriGajal?tab=repositories"
-}
-function linkedin(){
-    window.location.href="https://www.linkedin.com/in/ushasri-gajal-59b09324b/x"
-}
-function moviebut(){
-    window.location.href="https://ushasrigajal.github.io/MovieApi/"
-}
-function moviecode(){
-    window.location.href="https://github.com/UshasriGajal/MovieApi.git"
-}
-function currencybut(){
-    window.location.href="https://ushasrigajal.github.io/CurrencyConverter/"
-}
-function currencycode(){
-    window.location.href="https://github.com/UshasriGajal/CurrencyConverter.git"
-}
-function ticbut(){
-    window.location.href="https://ushasrigajal.github.io/TicTacToeGame/"
-}
-function ticcode(){
-    window.location.href="https://github.com/UshasriGajal/TicTacToeGame.git"
-}
-function Typebut(){
-    window.location.href="https://ushasrigajal.github.io/Typing_Test/"
-}
-function Typecode(){
-    window.location.href="https://github.com/UshasriGajal/Typing_Test.git"
-}
-function Zomatobut(){
-    window.location.href="https://cjz648.csb.app/"
-}
-function Zomatocode(){
-    window.location.href="https://codesandbox.io/p/sandbox/zomato-cjz648"
-}
-function navitem(){
-    console.log("yes")
-    var x = document.getElementById("portbut");
-    var divi=document.createElement("div")
-    divi.setAttribute("class","divi")
-  if (x.style.display === "block") {
-    x.style.display = "none";
+
+// $(document).ready(function(){
+//    $('.navbar-nav .nav-link').on('click', function(){
+//       $('.navbar-collapse').collapse('hide');
+//    });
+//  });
+
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
   } else {
-    x.style.display = "block";
+    mybutton.style.display = "none";
   }
 }
-// var elee=document.getElementById("projects").innerText
-// window.onload = function(){
-//     document.getElementById("projects").style.width="1100px"
-//     document.getElementById("projects").style.height="800px"
 
-    // project.innerText="hello"
-    // var image=document.createAttribute("img")
-    // image.src
-    // project.setAttribute("class","projectsdiv")
-    // var divi=document.createElement("div")
-    // divi.setAttribute("class","divi")
-    // project.append(divi)
-    // // document.body.append(project)
-    // console.log("yes")
-// }
-// function Resumebut(){
-
-// }
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
