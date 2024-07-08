20a220bdc83133285847cab9b5da8b1db42455db
UshasriGajal <126794491+UshasriGajal@users.noreply.github.com>
2024-07-08 10:31:58 +0530

    Update Portfolio.js

--- Portfolio.js
+++ Portfolio.js
@@ -1,10 +1,3 @@
-
-// $(document).ready(function(){
-//    $('.navbar-nav .nav-link').on('click', function(){
-//       $('.navbar-collapse').collapse('hide');
-//    });
-//  });
-
 let mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
